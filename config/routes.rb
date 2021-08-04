71a41679569380e823d4b46cde25b946618e3151
springkyu <seika.k.r0610@gmail.com>
2021-08-04 14:14:44 +0000

    [ADD]users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  resources :books
+  resources :users
 
 end
