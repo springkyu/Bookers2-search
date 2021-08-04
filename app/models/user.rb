6cd2e1094df52796badd34d133ae5a72c5bbd16e
springkyu <seika.k.r0610@gmail.com>
2021-08-04 14:35:30 +0000

    [UPDATE]user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :name, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
   has_many :books, dependent: :destroy
-  attachment :profile_ima
+  attachment :profile_image
 end
